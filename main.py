d529e485152d1574a8641af4aeb76e2b0f4c28d7
MythiliPalanisamy <102500645+MythiliPalanisamy@users.noreply.github.com>
2023-07-20 16:20:29 +0200

    updating main.py

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-"""import sys
-import os
-import pandas as pd
-"""
-# the absolute path of the "parts" directory
-#parts_dir = os.path.abspath(r"C:\Users\Karthick Palanivel\Documents\GitHub\analysing-data\src\model building\parts.py")
-
-# Adding the "parts" directory to sys.path
-#sys.path.append(parts_dir)
-
 from src.data_cleaning import run_data_cleaning
 from src.split_format import run_split_format
 from src.models import run_models
+
+# getting regression values from main.py
 
 path = 'data/final.csv'
 
